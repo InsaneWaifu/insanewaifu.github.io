3879b4742ae248a6636c4ad44dda6d5db7a91ad9
InsaneWaifu <superblastermag5@gmail.com>
2022-11-05 00:48:17 +0000

    collab

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,83 @@
 import {syllable} from 'syllable'
 import * as monaco from 'monaco-editor'
 import * as cssgen from 'css-generator'
+import * as Y from 'yjs'
+import { WebrtcProvider } from 'y-webrtc'
+import { MonacoBinding } from 'y-monaco'
 
+const ydocument = new Y.Doc()
+ydocument.getText(ydocument.clientID.toString()).insert(0, window.prompt("Enter name"))
+
+const provider = new WebrtcProvider("thebestparodyeditor", ydocument, {signaling:  ['wss://signaling.yjs.dev'] })
+const type = ydocument.getText('monaco')
+const type2 = ydocument.getText('orig')
 
 const conf = {
     snippetSuggestions: "none",
     quickSuggestions: false
 }
 
+function hashCode(str) { // java String#hashCode
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+       hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+} 
 
-var original_lyrics = monaco.editor.create(document.getElementById('container1'), {...conf, value: localStorage.getItem("source")});
+function intToRGB(i){
+    var c = (i & 0x00FFFFFF)
+        .toString(16)
+        .toUpperCase();
 
-var editor = monaco.editor.create(document.getElementById('container2'), {...conf, value: localStorage.getItem("parody")});
+    return "00000".substring(0, 6 - c.length) + c;
+}
 
+
+
+var editor = monaco.editor.create(document.getElementById('container2'), {...conf, /*value: localStorage.getItem("parody")*/});
+
+const monacoBinding = new MonacoBinding(type, editor.getModel(), new Set([editor]), provider.awareness)
+
+provider.awareness.on("update", () => {
+    let css = cssgen.create()
+    provider.awareness.states.forEach((value, key) => {
+        let col = intToRGB(hashCode(key.toString()))
+        css.addRule(`.yRemoteSelection-${key}`, {
+            "z-index": 9999,
+            "background-color": `#${col}7F`
+        })
+        css.addRule(`.yRemoteSelectionHead-${key}`, {
+            "z-index": 9999,
+            position: "absolute",
+            "border-left": `#${col} solid 2px`,
+            "border-top": `#${col} solid 2px`,
+            "border-bottom": `#${col} solid 2px`,
+            height: "100%",
+            "box-sizing": "border-box"
+        })
+        css.addRule(`.yRemoteSelectionHead-${key}::after`, {
+            position: "absolute",
+            "border": `3px solid #${col}`,
+            "border-radius": "4px",
+            "color": "white",
+            "background-color": `#${col}E6`,
+            "top": "15px",
+            content: "\"" + ydocument.getText(key.toString()).toString() + "\"",
+            display: "block"
+        })
+    })
+    document.getElementById("gencursor").innerHTML = css.getOutput()
+
+})
+
+var original_lyrics = monaco.editor.create(document.getElementById('container1'), {...conf, /*value: localStorage.getItem("source")*/});
+const monacoBinding2 = new MonacoBinding(type2, original_lyrics.getModel(), new Set([original_lyrics]), provider.awareness)
+
+if (provider.awareness.states.size < 2) {
+    original_lyrics.setValue(localStorage.getItem("source"))
+    editor.setValue(localStorage.getItem("parody"))
+}
 
 var decorations = editor.createDecorationsCollection([])
 
